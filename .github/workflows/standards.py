4c0139d37e19223e992c70291ab7e462edd0d254
Jan Garaj <jan.garaj@gmail.com>
2024-12-23 21:47:20 +0000

    Dashboard EBS update

--- .github/workflows/standards.py
+++ .github/workflows/standards.py
@@ -13,7 +13,8 @@ validated_dashboards = [
    "aws-codebuild/aws-codebuild.json",
    "aws-cognito/aws-cognito.json",
    "aws-direct-connect/aws-direct-connect.json",
-   "aws-dynamodb/aws-dynamodb.json"
+   "aws-dynamodb/aws-dynamodb.json",
+   "aws-ebs/aws-ebs.json"
 ]
 
 standard_footer = "<a target=\"_blank\" href=\"http://www.monitoringartist.com\" title=\"Dashboard maintained by Monitoring Artist - DevOps / Docker / Kubernetes / AWS ECS / Google GCP / Zabbix / Zenoss / Terraform / Monitoring\"><img src=\"https://monitoringartist.github.io/monitoring-artist-logo-grafana.png\" height=\"30px\" /></a> | \n<a target=\"_blank\" href=\"https://docs.aws.amazon.com/transfer/latest/userguide/monitoring.html\">AWS CloudWatch Transfer Family documentation</a> | \n<a target=\"_blank\" href=\"https://grafana.com/dashboards/20008\">Installed from Grafana.com dashboards</a>"
