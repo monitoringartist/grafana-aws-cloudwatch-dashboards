863195224bb8b337350556c6b1964ea17d4383d3
Jan Garaj <jan.garaj@gmail.com>
2024-12-24 17:51:19 +0000

    Dashboard Elasticache Redis update

--- .github/workflows/standards.py
+++ .github/workflows/standards.py
@@ -18,7 +18,8 @@ validated_dashboards = [
    "aws-ec2/aws-ec2.json",
    "aws-ecs/aws-ecs.json",
    "aws-efs/aws-efs.json",
-   "aws-eks/aws-eks.json"
+   "aws-eks/aws-eks.json",
+   "aws-elasticache/aws-elasticache-redis.json"
 ]
 
 standard_footer = "<a target=\"_blank\" href=\"http://www.monitoringartist.com\" title=\"Dashboard maintained by Monitoring Artist - DevOps / Docker / Kubernetes / AWS ECS / Google GCP / Zabbix / Zenoss / Terraform / Monitoring\"><img src=\"https://monitoringartist.github.io/monitoring-artist-logo-grafana.png\" height=\"30px\" /></a> | \n<a target=\"_blank\" href=\"https://docs.aws.amazon.com/transfer/latest/userguide/monitoring.html\">AWS CloudWatch Transfer Family documentation</a> | \n<a target=\"_blank\" href=\"https://grafana.com/dashboards/20008\">Installed from Grafana.com dashboards</a>"
